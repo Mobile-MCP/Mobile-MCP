0c20fdf6a3c20980d6f43430cf0458e7d61ef4e6
Tomer Rosenfeld <tomer.rosenfeld@mixtiles.com>
2025-05-29 18:01:18 -0400

    removed old file

--- shared/MMCPCore/mmcp/src/commonMain/kotlin/io/rosenpin/mcp/mmcpcore/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.rosenpin.mmcp.mmcpcore
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
